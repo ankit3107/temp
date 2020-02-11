100b683a8cafc38f196191f60b5cf5031908947a
Ankit Kothari <kothariankit31@gmail.com>
2020-02-12 00:08:02 +0530

    Reversing array

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-print("hello")
+def reverseArray(a):
+    len_a = len(a)
+    reverse_list = []
+    while len_a > 0:
+        len_a -= 1
+        print(a[len_a])
+        reverse_list.append(a[len_a])
+
+    return reverse_list
 
 
-def testfunc():
-    print("test")
+reverseArray([1, 2, 3])
 
-
-print("yo")
-print("Added a line")
-print("second line")
